a6b3580b2dc5bf4f1bddc4629fb455cba6e4dd58
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2023-01-30 09:25:09 +0300

    Create Assessment.java

---
+++ input-output-streams/src/Assessment.java
@@ -0,0 +1,28 @@
+public class Assessment
+{
+    private Course _course;
+    private Student _student;
+    private Integer _score;
+
+    public Assessment(Course course, Student student, Integer score) {
+        _course = course;
+        _student = student;
+        _score = score;
+    }
+
+    public Course getCourse() {
+        return _course;
+    }
+
+    public Student getStudent() {
+        return _student;
+    }
+
+    public void set_score(Integer _score) {
+        this._score = _score;
+    }
+
+    public Integer get_score() {
+        return _score;
+    }
+}
