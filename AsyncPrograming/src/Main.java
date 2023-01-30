8ca13de0b9e406adbfcf71db867e3ca86d3b9383
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2023-01-30 14:44:20 +0300

    Create functions for stream

--- AsyncPrograming/src/Main.java
+++ AsyncPrograming/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
+    }
+    public int SumFunction(int a, int b)
+    {
+        return a + b;
+    }
+    public int MultiplyFunction(int a, int b)
+    {
+        return a * b;
     }
 }
