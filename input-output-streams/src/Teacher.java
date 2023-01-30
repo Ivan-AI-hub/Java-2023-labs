500610ae4d82c62e99169c73bf3e549fdb5d80d9
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2023-01-30 09:12:34 +0300

    Create teacher class

---
+++ input-output-streams/src/Teacher.java
@@ -0,0 +1,16 @@
+public class Teacher
+{
+    private String Name;
+
+    public Teacher(String name)
+    {
+        Name = name;
+    }
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
