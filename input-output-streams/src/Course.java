ad532fec8a142964af22bae484fb3173b07f4e98
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2023-01-30 09:13:36 +0300

    Create Course.java

---
+++ input-output-streams/src/Course.java
@@ -0,0 +1,16 @@
+public class Course
+{
+    private String Name;
+
+    public Course(String name)
+    {
+        Name = name;
+    }
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
