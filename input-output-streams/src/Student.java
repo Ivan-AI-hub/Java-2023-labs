4e2d015bdd4dc8485273c4a4cc66d43267e2b39b
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2023-01-30 09:13:06 +0300

    Create Student.java

---
+++ input-output-streams/src/Student.java
@@ -0,0 +1,16 @@
+public class Student
+{
+    private String Name;
+
+    public Student(String name)
+    {
+        Name = name;
+    }
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
