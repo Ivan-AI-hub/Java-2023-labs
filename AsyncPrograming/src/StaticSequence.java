3dc79b28956fde3e5503f0cbc0a26e32b0673736
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2023-01-31 11:17:35 +0300

    Create StaticSequence.java

---
+++ AsyncPrograming/src/StaticSequence.java
@@ -0,0 +1,11 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class StaticSequence
+{
+    public static ArrayDeque<Integer> Sequence;
+    static
+    {
+        Sequence = new ArrayDeque();
+    }
+}
