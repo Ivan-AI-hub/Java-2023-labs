d8d95a956b63556a6c9ffa647c356e71a6bf5753
Ivan Andreevets <73757236+Ivan-AI-hub@users.noreply.github.com>
2023-02-01 09:13:56 +0300

    Code clear

--- test-web-service/src/my/ws/CalculateWebService.java
+++ test-web-service/src/my/ws/CalculateWebService.java
@@ -2,7 +2,7 @@ package my.ws;
 
 import javax.jws.WebService;
 
-@WebService(endpointInterface = "my.ws.IMyWebService")
+@WebService(endpointInterface = "my.ws.ICalculateWebService")
 public class CalculateWebService implements ICalculateWebService
 {
     @Override
@@ -19,10 +19,12 @@ public class CalculateWebService implements ICalculateWebService
     public int multiply(int a, int b) {
         return a * b;
     }
+
     @Override
     public int division(int a, int b) {
         return a / b;
     }
+
     @Override
     public int greatestCommonDivisor(int a, int b) {
         if (b == 0) {
